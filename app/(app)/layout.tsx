764033d062b047edd2721b759b077b4597dd3eed
Aaraj Bhattarai <53218772+aarajbhattarai@users.noreply.github.com>
2025-09-27 20:37:46 +0545

    links modified

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -15,17 +15,17 @@ export default async function AppLayout({ children }: AppLayoutProps) {
         <a
           target="_blank"
           rel="noopener noreferrer"
-          href="https://livekit.io"
+          href="https://refobe.com"
           className="scale-100 transition-transform duration-300 hover:scale-110"
         >
           {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src={logo} alt={`${companyName} Logo`} className="block size-6 dark:hidden" />
+          {/* <img src={logo} alt={`${companyName} Logo`} className="block size-6 dark:hidden" /> */}
           {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
+          {/* <img
             src={logoDark ?? logo}
             alt={`${companyName} Logo`}
             className="hidden size-6 dark:block"
-          />
+          /> */}
         </a>
       </header>
       {children}
